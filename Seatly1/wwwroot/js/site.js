1fa639be74449825540381139b6dae38d53da0ae
throat <ken331278910@gmail.com>
2024-04-02 13:46:49 +0800

    新增layout點數專區導航列

--- Seatly1/wwwroot/js/site.js
+++ Seatly1/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+    $("#pointsNav").on("mouseenter", function () {
+        $(this).addClass("show");
+        $(this).attr("aria-expanded","true");
+        $("#pointsDropdown").addClass("show");
+        $("#pointsDropdown").attr("data-bs-popper","none")
+    });
+    $("#pointsNav").on("mouseout", function () {
+        $(this).removeClass("show");
+        $(this).attr("aria-expanded", "false");
+        $("#pointsDropdown").removeClass("show");
+        $("#pointsDropdown").attr("data-bs-popper","")
+    });
+})
