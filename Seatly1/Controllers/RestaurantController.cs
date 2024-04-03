c012139d10f5d4f727e5f081df092b1a297e4d29
tohousanae <122202405+tohousanae@users.noreply.github.com>
2024-04-03 13:43:39 +0800

    建立測試用餐廳api

---
+++ Seatly1/Controllers/RestaurantController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Seatly1.Models;
+
+namespace Seatly1.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        SeatlyContext _context;
+        public RestaurantController(SeatlyContext context)
+            {
+            _context = context;
+            }
+
+        [HttpGet]
+        public string Greet(string Name) 
+        {
+            return $"Hello,{Name}!"; 
+        }
+
+        [HttpPost,ActionName("Greet")]
+        public string PostGreet(string Name) 
+        {
+            return $"Hello,{Name}!";
+        }
+
+        [HttpPost()]
+        public string FetchPostGreet([FromBody]Parameter p)
+        {
+            return $"Hello,{p.Name}!";
+        }
+
+        //POST: /Ajax/CheckRestaurantName
+        [HttpPost()]
+        public string CheckRestaurantName(string RestaurantName)
+        {
+            bool Exists = _context.Restaurants.Any(emp => emp.RestaurantName == RestaurantName);
+            return Exists ? "true" : "false" ;   
+        }
+
+        [HttpPost()]
+        public string FetchCheckRestaurantName(string RestaurantName)
+        {
+            bool Exists = _context.Restaurants.Any(emp => emp.RestaurantName == RestaurantName);
+            return Exists ? "true" : "false";
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
