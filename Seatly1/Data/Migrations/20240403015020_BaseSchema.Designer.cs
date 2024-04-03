e753a1a9d0c01f37feff032c83202913f5024744
throat <ken331278910@gmail.com>
2024-04-03 11:08:54 +0800

    導覽列配色更改

--- Seatly1/Data/Migrations/20240403015020_BaseSchema.Designer.cs
+++ Seatly1/Data/Migrations/20240403015020_BaseSchema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Seatly1.Data;
 
@@ -11,9 +12,11 @@ using Seatly1.Data;
 namespace Seatly1.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240403015020_BaseSchema")]
+    partial class BaseSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Seatly1/wwwroot/js/site.js
+++ Seatly1/wwwroot/js/site.js
@@ -4,6 +4,7 @@
 // Write your JavaScript code.
 
 $(function () {
+    /*點數專區導覽列hover開始*/
     var timer;
     $("#pointsNav, #pointsDropdown").on("mouseenter", function () {
         clearTimeout(timer);
@@ -21,4 +22,5 @@ $(function () {
             $("#pointsDropdown").attr("data-bs-popper", "");
         }, 100); // 這裡可以調整延遲的時間
     });
+    /*點數專區導覽列hover結束*/
 })
