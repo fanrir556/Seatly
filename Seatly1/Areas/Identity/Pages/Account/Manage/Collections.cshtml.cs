a6b63176badb4ac19851373420a0fe9e487319b5
Yuci <t0955935713@gmail.com>
2024-04-04 22:55:47 +0800

    0404-add Friends & Collections

---
+++ Seatly1/Areas/Identity/Pages/Account/Manage/Collections.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Seatly1.Areas.Identity.Pages.Account.Manage
+{
+    public class CollectionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Seatly1/Areas/Identity/Pages/Account/Manage/Friends.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace Seatly1.Areas.Identity.Pages.Account.Manage
+{
+    public class FriendsModel : PageModel
+    {
+        public void OnGet()
+        {
+            // ?理 GET ?求的??，例如??据?加?好友列表?据等
+        }
+    }
+}
