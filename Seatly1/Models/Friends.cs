5d6ef358a55a3fcfb98fc0f3d0fcdf6225399d04
Yuci <t0955935713@gmail.com>
2024-04-07 12:16:19 +0800

    0407-add Friends資料表

---
+++ Seatly1/Models/Friends.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Seatly1.Models
+{
+    public class Friends
+    {
+        [Key]
+        public int FlowID { get; set; }
+
+        public int? UserID { get; set; }
+
+        [Display(Name = "使用者帳號")]
+        public string? UserName { get; set; }
+
+        [Display(Name = "好友帳號")]
+        public string? FriendUserName { get; set; }
+    }
+}
