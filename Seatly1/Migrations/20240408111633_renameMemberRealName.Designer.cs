81e6da0e437ba790fca23bf79a64954c115d9b00
Yuci <t0955935713@gmail.com>
2024-04-08 20:35:02 +0800

    0408-能動!!備份-AspNetRoles有改

--- Seatly1/Migrations/20240408111633_renameMemberRealName.Designer.cs
+++ Seatly1/Migrations/20240408111633_renameMemberRealName.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Seatly1.Data;
 
 #nullable disable
 
-namespace Seatly1.Data.Migrations
+namespace Seatly1.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240408111633_renameMemberRealName")]
+    partial class renameMemberRealName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Seatly1/Migrations/20240408111633_renameMemberRealName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Seatly1.Migrations
+{
+    /// <inheritdoc />
+    public partial class renameMemberRealName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MemberAccount",
+                table: "AspNetUsers",
+                newName: "MemberRealName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MemberRealName",
+                table: "AspNetUsers",
+                newName: "MemberAccount");
+        }
+    }
+}
