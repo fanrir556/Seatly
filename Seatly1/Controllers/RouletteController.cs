a2c65b2e8b415582d0a11c8306182f1686ec69ed
T70 <qcc850369@gmail.com>
2024-04-10 14:20:54 +0800

    合併t70

---
+++ Seatly1/Controllers/RouletteController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Seatly1.Models;
+using System.Diagnostics;
+
+namespace Seatly1.Controllers
+{
+    public class RouletteController : Controller
+    {
+        private readonly ILogger<RouletteController> _loggerRoulette;
+        public RouletteController(ILogger<RouletteController> logger)
+        {
+            _loggerRoulette = logger;
+        }
+        public IActionResult RouletteIndex()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+}
--- Seatly1/wwwroot/js/site.js
+++ Seatly1/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+    /*點數專區導覽列hover開始*/
+    var timer;
+    $("#pointsNav, #pointsDropdown").on("mouseenter", function () {
+        clearTimeout(timer);
+        $("#pointsNav").addClass("show");
+        $("#pointsNav").attr("aria-expanded", "true");
+        $("#pointsDropdown").addClass("show");
+        $("#pointsDropdown").attr("data-bs-popper", "none");
+    });
+
+    $("#pointsNav, #pointsDropdown").on("mouseleave", function () {
+        timer = setTimeout(function () {
+            $("#pointsNav").removeClass("show");
+            $("#pointsNav").attr("aria-expanded", "false");
+            $("#pointsDropdown").removeClass("show");
+            $("#pointsDropdown").attr("data-bs-popper", "");
+        }, 100); // 這裡可以調整延遲的時間
+    });
+    /*點數專區導覽列hover結束*/
+})
+
