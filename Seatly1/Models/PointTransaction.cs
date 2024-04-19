420cebdea4301495ae0c2b05e4bae3b395bd5c2c
T70 <qcc850369@gmail.com>
2024-04-19 09:37:00 +0800

    0419/點數區displayname修正2

--- Seatly1/Models/PointTransaction.cs
+++ Seatly1/Models/PointTransaction.cs
@@ -2,18 +2,24 @@
 #nullable disable
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Seatly1.Models;
 
 public partial class PointTransaction
 {
+    [Display(Name = "交易編號")]
     public int Id { get; set; }
 
+    [Display(Name = "會員編號")]
     public string MemberId { get; set; }
 
+    [Display(Name = "優惠券編號")]
     public int? ProductId { get; set; }
 
+    [Display(Name = "交易時間")]
     public DateTime? TransactionDate { get; set; }
 
+    [Display(Name = "優惠券狀態")]
     public bool? Active { get; set; }
 }
