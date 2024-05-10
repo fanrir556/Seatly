4a756429742988c9eb1f641dd6d278e1b861f20c
Yuci <t0955935713@gmail.com>
2024-05-10 18:35:25 +0800

    0510-首頁搜尋vue

--- Seatly1/Controllers/CategoryController.cs
+++ Seatly1/Controllers/CategoryController.cs
@@ -1,14 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Seatly1.Models;
+using System.Linq;
 
 namespace Seatly1.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly SeatlyContext _context;
+
+        public CategoryController(SeatlyContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult categoryIndex()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetActivitiesByHashtag (string? hashtag)
+        {
+            var activities = await _context.NotificationRecords.Where(a => 
+                                a.HashTag1.Contains(hashtag) || 
+                                a.HashTag2.Contains(hashtag) ||
+                                a.HashTag3.Contains(hashtag) ||
+                                a.HashTag4.Contains(hashtag) ||
+                                a.HashTag5.Contains(hashtag)).ToListAsync();
+            return Json(activities);
         }
     }
 }
