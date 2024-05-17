ed4bd79c326b553fce50e2d3ed2a60d91874762f
T70 <qcc850369@gmail.com>
2024-05-17 16:06:41 +0800

    修正

--- Seatly1/Models/CollectionItem.cs
+++ Seatly1/Models/CollectionItem.cs
@@ -2,11 +2,13 @@
 #nullable disable
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Seatly1.Models;
 
 public partial class CollectionItem
 {
+    [Key]
     public int SerialId { get; set; }
 
     public string UserId { get; set; }
